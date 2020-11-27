b846a5ffa8d2a79732b66903609fe5187faebf4a
jakub skory <jakub.skory@atos.net>
2020-11-28 00:44:25 +0100

    Added API

---
+++ api/app.py
@@ -0,0 +1,68 @@
+from flask import (Flask, render_template)
+from flask import request
+from flask import Response
+from flask_sqlalchemy import SQLAlchemy
+from flask import jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite3'
+db = SQLAlchemy(app)
+CORS(app)
+
+
+class Subject(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    address_text = db.Column(db.String(80), unique=False, nullable=False)
+    address_latitude = db.Column(db.Float, unique=False, nullable=False)
+    address_longitude = db.Column(db.Float, unique=False, nullable=False)
+    contact_info = db.Column(db.String(80), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Subject %r>' % self.name
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializable format"""
+        return {
+            'id': self.id,
+            'name': self.name,
+            'address_text': self.address_text,
+            'address_latitude': self.address_latitude,
+            'address_longitude': self.address_longitude,
+            'contact_info': self.contact_info,
+        }
+
+
+@app.route('/')
+def hello_world():
+    return render_template("index.html")
+
+
+@app.route('/api/subject', methods=['GET', 'PUT'])
+def subject():
+    if request.method == 'GET':
+        return jsonify([i.serialize for i in Subject.query.all()])
+    if request.method == 'POST':
+        # TODO: you should validate things
+        my_subject = Subject(
+            name=request.form['name'],
+            address_text=request.form['address_text'],
+            address_latitude=request.form['address_latitude'],
+            address_longitude=request.form['address_longitude'],
+            contact_info=request.form['contact_info']
+        )
+        db.session.add(my_subject)
+        db.session.commit()  # TODO: we should check if we've succeeded, but fuck it
+        return Response("{'status':'created'}", status=201, mimetype='application/json')
+    if request.method == 'DELETE':
+
+
+@app.route('/api/dupaslonia', methods=['GET'])
+def dupaslonia():
+    return jsonify({"alamakota": "test"})
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ api/schema.sql
@@ -0,0 +1,26 @@
+CREATE TABLE subject (
+	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	name TEXT NOT NULL,
+	address_text TEXT NOT NULL,
+	address_latitude REAL,
+	address_longitude REAL,
+	contact_info TEXT NOT NULL
+);
+
+CREATE TABLE item (
+	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	"type" TEXT NOT NULL,
+	state TEXT NOT NULL,
+	owner INTEGER NOT NULL,
+	CONSTRAINT item_FK FOREIGN KEY (owner) REFERENCES subject(id)
+);
+
+CREATE TABLE demand (
+	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	who INTEGER NOT NULL,
+	what INTEGER NOT NULL,
+	how_many INTEGER,
+	what_in_exchange TEXT NOT NULL,
+	CONSTRAINT demand_FK FOREIGN KEY (who) REFERENCES subject(id),
+	CONSTRAINT demand_FK_1 FOREIGN KEY (what) REFERENCES item(id)
+);
