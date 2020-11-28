af808616d3e310e864ea9aa1383ed83d0d466a5b
jakub skory <jakub.skory@atos.net>
2020-11-28 17:18:34 +0100

    stash

--- api/app.py
+++ api/app.py
@@ -35,6 +35,27 @@ app.config['SWAGGER'] = {
 swagger = Swagger(app, template=template)
 
 
+class Demand(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    owner = db.Column(db.Integer)
+
+    direction_give = db.Column(db.Boolean)
+    type = db.Column(db.String)
+    condition = db.Column(db.String)
+    what_in_exchange = db.Column(db.String)
+
+    def __repr__(self):
+        return '<Demand %r>' % self.name
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializable format"""
+        return {
+            'id': self.id,
+        }
+
+
 class Subject(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=False, nullable=False)
@@ -106,6 +127,59 @@ class SubjectResource(Resource):
         my_subject = Subject.query.get(subject_id)
         if my_subject:
             db.session.delete(my_subject)
+            db.session.commit()
+            return json_response(status_=200, message="Deleted")
+        else:
+            return json_response(status_=404, message="Not Found")
+
+
+@api.resource('/api/demand', '/api/demand/')
+class DemandListResource(Resource):
+    @swag_from("docs/DemandResource/get.yml")
+    def get(self):
+        return json_response(status_=200, message="OK", data=[i.serialize for i in Demand.query.all()])
+
+    @swag_from("docs/DemandResource/post.yml")
+    def post(self):
+        # TODO: you should validate things but fuck it
+        parser = reqparse.RequestParser()
+        parser.add_argument('who', type=int, help='Id of subject')
+        parser.add_argument('direction_give', type=bool, help='Give or take (true = give, false = take')
+        parser.add_argument('type', type=str, help='What is type of item')
+        parser.add_argument('condition', type=str, help='What is a condition of item')
+        parser.add_argument('what_in_exchange', type=str, help='What you expect in exchange (can be nothing -> null)')
+        args = parser.parse_args(strict=True)
+
+        my_demand = Demand(
+            who=args['who'],
+            direction_give=args['direction_give'],
+            type=args['type'],
+            condition=args['condition'],
+            what_in_exchange=args['what_in_exchange']
+        )
+        db.session.add(my_demand)
+        db.session.commit()  # TODO: we should check if we've succeeded, but fuck it
+        return json_response(status_=201, message="Created", data=my_demand.serialize)
+
+
+@api.resource('/api/demand/<int:subject_id>')
+class DemandResource(Resource):
+    @swag_from("docs/DemandResource/get.yml")
+    def get(self, demand_id=None):
+        if demand_id:
+            my_demand = Demand.query.get(demand_id)
+            if my_demand:
+                return json_response(status_=200, message="OK", data=Demand.query.get(demand_id).serialize)
+            else:
+                return json_response(status_=404, message="Not Found")
+        else:
+            return json_response(status_=404, message="Not Found")
+
+    @swag_from("docs/DemandResource/delete.yml")
+    def delete(self, subject_id=None):
+        my_demand = Demand.query.get(subject_id)
+        if my_demand:
+            db.session.delete(my_demand)
             db.session.commit()
             return json_response(status_=200, message="Deleted")
         else:
