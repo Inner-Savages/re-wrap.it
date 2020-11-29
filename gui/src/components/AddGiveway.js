1f0ca36ef84d2e402c17b1e08b74c45964d673e6
jakub skory <jakub.skory@atos.net>
2020-11-29 04:58:51 +0100

    Added AddGiveaway

--- gui/src/components/AddGiveway.js
+++ gui/src/components/AddGiveway.js
@@ -3,203 +3,190 @@ import React, { Component } from 'react';
 class AddGiveway extends Component {
   render() {
     return (
-      <div className="row">
-        <div className="col-md-4 order-md-2 mb-4">
-          <h4 className="d-flex justify-content-between align-items-center mb-3">
-            <span className="text-muted">Your cart</span>
-            <span className="badge badge-secondary badge-pill">3</span>
-          </h4>
-          <ul className="list-group mb-3">
-            <li className="list-group-item d-flex justify-content-between lh-condensed">
-              <div>
-                <h6 className="my-0">Product name</h6>
-                <small className="text-muted">Brief description</small>
-              </div>
-              <span className="text-muted">$12</span>
-            </li>
-            <li className="list-group-item d-flex justify-content-between lh-condensed">
-              <div>
-                <h6 className="my-0">Second product</h6>
-                <small className="text-muted">Brief description</small>
-              </div>
-              <span className="text-muted">$8</span>
-            </li>
-            <li className="list-group-item d-flex justify-content-between lh-condensed">
-              <div>
-                <h6 className="my-0">Third item</h6>
-                <small className="text-muted">Brief description</small>
-              </div>
-              <span className="text-muted">$5</span>
-            </li>
-            <li className="list-group-item d-flex justify-content-between bg-light">
-              <div className="text-success">
-                <h6 className="my-0">Promo code</h6>
-                <small>EXAMPLECODE</small>
-              </div>
-              <span className="text-success">-$5</span>
-            </li>
-            <li className="list-group-item d-flex justify-content-between">
-              <span>Total (USD)</span>
-              <strong>$20</strong>
-            </li>
-          </ul>
+        <div className="container">
+          <div className="py-5 text-center">
+            <img className="d-block mx-auto mb-4" src="./images/2a.png" alt="" width="200" height="200"/>
+              <h2>Product form</h2>
+            <p className="lead">Here you can add few photos, and describe your product.</p>
+          </div>
 
-          <form className="card p-2">
-            <div className="input-group">
-              <input type="text" className="form-control" placeholder="Promo code"/>
-              <div className="input-group-append">
-                <button type="submit" className="btn btn-secondary">Redeem</button>
-              </div>
+          <div className="row">
+            <div className="col-md-4 order-md-2 mb-4">
+              <h4 className="d-flex justify-content-between align-items-center mb-3">
+                <span className="text-muted">Product photos</span>
+                <span className="badge badge-secondary badge-pill">3</span>
+              </h4>
+              <ul className="list-group mb-3">
+                <li className="list-group-item d-flex justify-content-between lh-condensed">
+                  <div>
+                    <h6 className="my-0">First image</h6>
+                    <small className="text-muted">Brief description</small>
+                  </div>
+                  <span className="text-muted">ADD</span>
+                </li>
+                <li className="list-group-item d-flex justify-content-between lh-condensed">
+                  <div>
+                    <h6 className="my-0">Second image</h6>
+                    <small className="text-muted">Brief description</small>
+                  </div>
+                  <span className="text-muted">ADD</span>
+                </li>
+                <li className="list-group-item d-flex justify-content-between lh-condensed">
+                  <div>
+                    <h6 className="my-0">Third image</h6>
+                    <small className="text-muted">Brief description</small>
+                  </div>
+                  <span className="text-muted">ADD</span>
+                </li>
+              </ul>
             </div>
-          </form>
+            <div className="col-md-8 order-md-1">
+              <h4 className="mb-3"></h4>
+              <form className="needs-validation" noValidate>
+                <div className="row">
+                  <div className="col-md-6 mb-3">
+                    <label htmlFor="productName">Product name</label>
+                    <input type="text" className="form-control" id="productName" placeholder="" value="" required/>
+                      <div className="invalid-feedback">
+                        Valid first name is required.
+                      </div>
+                  </div>
+                  <div className="col-md-4 mb-3">
+                    <label htmlFor="Category">Category</label>
+                    <select className="custom-select d-block w-100" id="Category" required>
+                      <option value="">Plastic bags</option>
+                      <option>Cartons</option>
+                      <option>Jars</option>
+                      <option>Bubble wrap</option>
+                      <option>Others</option>
+
+                    </select>
+                    <div className="invalid-feedback">
+                      Please provide a valid state.
+                    </div>
+                  </div>
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="username">Username</label>
+                  <div className="input-group">
+                    <div className="input-group-prepend">
+                      <span className="input-group-text">@</span>
+                    </div>
+                    <input type="text" className="form-control" id="username" placeholder="Username" required/>
+                      <div className="invalid-feedback">
+                        Your username is required.
+                      </div>
+                  </div>
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="email">Email <span className="text-muted">(Optional)</span></label>
+                  <input type="email" className="form-control" id="email" placeholder="you@example.com"/>
+                    <div className="invalid-feedback">
+                      Please enter a valid email address for shipping updates.
+                    </div>
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="address">Adress <span className="text-muted">(Optional)</span></label>
+                  <input type="text" className="form-control" id="address" placeholder="1234 Main St"/>
+                    <div className="invalid-feedback">
+                      Please enter your shipping address.
+                    </div>
+                </div>
+
+                <div className="mb-3">
+                  <label htmlFor="address2">Phone number </label>
+                  <input type="text" className="form-control" id="address2" placeholder="+48 123 456 789"/>
+                </div>
+
+                <div className="row">
+                  <div className="col-md-5 mb-3">
+                    <label htmlFor="City">City</label>
+                    <input type="text" className="form-control" id="City" placeholder="" required/>
+                      <div className="invalid-feedback">
+                        Please select a valid City.
+                      </div>
+                  </div>
+                  <div className="col-md-4 mb-3">
+                    <label htmlFor="state">State</label>
+                    <select className="custom-select d-block w-100" id="state" required>
+                      <option value="">Choose...</option>
+                      <option>dolnośląskie</option>
+                      <option>kujawsko-pomorskie</option>
+                      <option>lubelskie</option>
+                      <option>lubuskie</option>
+                      <option>łódzkie</option>
+                      <option>małopolskie</option>
+                      <option>mazowieckie</option>
+                      <option>opolskie</option>
+                      <option>podkarpackie</option>
+                      <option>podlaskie</option>
+                      <option>pomorskie</option>
+                      <option>śląskie</option>
+                      <option>świętokrzyskie</option>
+                      <option>warmińsko mazurskie</option>
+                      <option>wielkopolskie</option>
+                      <option>zachodniopomorskie</option>
+                    </select>
+                    <div className="invalid-feedback">
+                      Please provide a valid state.
+                    </div>
+                  </div>
+                  <div className="col-md-3 mb-3">
+                    <label htmlFor="zip">Zip</label>
+                    <input type="text" className="form-control" id="zip" placeholder="" required/>
+                      <div className="invalid-feedback">
+                        Zip code required.
+                      </div>
+                  </div>
+                </div>
+                <h4 className="mb-3">Item condition</h4>
+                <div className="d-block my-3">
+                  <div className="custom-control custom-checkbox">
+                    <input type="checkbox" className="custom-control-input" id="new-info"/>
+                      <label className="custom-control-label" htmlFor="new-info">New</label>
+                  </div>
+                  <div className="custom-control custom-checkbox">
+                    <input type="checkbox" className="custom-control-input" id="used-info"/>
+                      <label className="custom-control-label" htmlFor="used-info">Used</label>
+                  </div>
+                  <div className="custom-control custom-checkbox">
+                    <input type="checkbox" className="custom-control-input" id="incomplete-info"/>
+                      <label className="custom-control-label" htmlFor="incomplete-info">Incomplete</label>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-md-6 mb-3">
+                    <label htmlFor="cc-name">Quantity</label>
+                    <input type="text" className="form-control" id="cc-name" placeholder="" required/>
+                      <div className="invalid-feedback">
+                        Quantity is required.
+                      </div>
+                  </div>
+                  <div className="col-md-6 mb-3">
+                    <label htmlFor="cc-number">Dimensions</label>
+                    <input type="text" className="form-control" id="cc-number" placeholder=""/>
+                      <div className="invalid-feedback">
+                        Dimensions are not required.
+                      </div>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-md-4 mb-4">
+                    <label htmlFor="cc-expiration">Payment</label>
+                    <input type="text" className="form-control" id="cc-expiration" placeholder=""/>
+                      <div className="invalid-feedback">
+                        Bonus date required
+                      </div>
+                  </div>
+                </div>
+                <hr className="mb-8"/>
+                  <button className="btn btn-primary btn-lg btn-info" type="submit">Add item</button>
+              </form>
+            </div>
+          </div>
         </div>
-        <div className="col-md-8 order-md-1">
-          <h4 className="mb-3">Billing address</h4>
-          <form className="needs-validation" novalidate>
-            <div className="row">
-              <div className="col-md-6 mb-3">
-                <label for="firstName">First name</label>
-                <input type="text" className="form-control" id="firstName" placeholder="" value="" required/>
-                <div className="invalid-feedback">
-                  Valid first name is required.
-                </div>
-              </div>
-              <div className="col-md-6 mb-3">
-                <label for="lastName">Last name</label>
-                <input type="text" className="form-control" id="lastName" placeholder="" value="" required/>
-                <div className="invalid-feedback">
-                  Valid last name is required.
-                </div>
-              </div>
-            </div>
-
-            <div className="mb-3">
-              <label for="username">Username</label>
-              <div className="input-group">
-                <div className="input-group-prepend">
-                  <span className="input-group-text">@</span>
-                </div>
-                <input type="text" className="form-control" id="username" placeholder="Username" required/>
-                <div className="invalid-feedback" style="width: 100%;">
-                  Your username is required.
-                </div>
-              </div>
-            </div>
-
-            <div className="mb-3">
-              <label for="email">Email <span className="text-muted">(Optional)</span></label>
-              <input type="email" className="form-control" id="email" placeholder="you@example.com"/>
-              <div className="invalid-feedback">
-                Please enter a valid email address for shipping updates.
-              </div>
-            </div>
-
-            <div className="mb-3">
-              <label for="address">Address</label>
-              <input type="text" className="form-control" id="address" placeholder="1234 Main St" required/>
-              <div className="invalid-feedback">
-                Please enter your shipping address.
-              </div>
-            </div>
-
-            <div className="mb-3">
-              <label for="address2">Address 2 <span className="text-muted">(Optional)</span></label>
-              <input type="text" className="form-control" id="address2" placeholder="Apartment or suite"/>
-            </div>
-
-            <div className="row">
-              <div className="col-md-5 mb-3">
-                <label for="country">Country</label>
-                <select className="custom-select d-block w-100" id="country" required>
-                  <option value="">Choose...</option>
-                  <option>United States</option>
-                </select>
-                <div className="invalid-feedback">
-                  Please select a valid country.
-                </div>
-              </div>
-              <div className="col-md-4 mb-3">
-                <label for="state">State</label>
-                <select className="custom-select d-block w-100" id="state" required>
-                  <option value="">Choose...</option>
-                  <option>California</option>
-                </select>
-                <div className="invalid-feedback">
-                  Please provide a valid state.
-                </div>
-              </div>
-              <div className="col-md-3 mb-3">
-                <label for="zip">Zip</label>
-                <input type="text" className="form-control" id="zip" placeholder="" required/>
-                <div className="invalid-feedback">
-                  Zip code required.
-                </div>
-              </div>
-            </div>
-            <hr className="mb-4"/>
-            <div className="custom-control custom-checkbox">
-              <input type="checkbox" className="custom-control-input" id="same-address"/>
-              <label className="custom-control-label" for="same-address">Shipping address is the same as my billing address</label>
-            </div>
-            <div className="custom-control custom-checkbox">
-              <input type="checkbox" className="custom-control-input" id="save-info"/>
-              <label className="custom-control-label" for="save-info">Save this information for next time</label>
-            </div>
-            <hr className="mb-4"/>
-
-            <h4 className="mb-3">Payment</h4>
-
-            <div className="d-block my-3">
-              <div className="custom-control custom-radio">
-                <input id="credit" name="paymentMethod" type="radio" className="custom-control-input" checked required/>
-                <label className="custom-control-label" for="credit">Credit card</label>
-              </div>
-              <div className="custom-control custom-radio">
-                <input id="debit" name="paymentMethod" type="radio" className="custom-control-input" required/>
-                <label className="custom-control-label" for="debit">Debit card</label>
-              </div>
-              <div className="custom-control custom-radio">
-                <input id="paypal" name="paymentMethod" type="radio" className="custom-control-input" required/>
-                <label className="custom-control-label" for="paypal">PayPal</label>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-md-6 mb-3">
-                <label for="cc-name">Name on card</label>
-                <input type="text" className="form-control" id="cc-name" placeholder="" required/>
-                <small className="text-muted">Full name as displayed on card</small>
-                <div className="invalid-feedback">
-                  Name on card is required
-                </div>
-              </div>
-              <div className="col-md-6 mb-3">
-                <label for="cc-number">Credit card number</label>
-                <input type="text" className="form-control" id="cc-number" placeholder="" required/>
-                <div className="invalid-feedback">
-                  Credit card number is required
-                </div>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-md-3 mb-3">
-                <label for="cc-expiration">Expiration</label>
-                <input type="text" className="form-control" id="cc-expiration" placeholder="" required/>
-                <div className="invalid-feedback">
-                  Expiration date required
-                </div>
-              </div>
-              <div className="col-md-3 mb-3">
-                <label for="cc-cvv">CVV</label>
-                <input type="text" className="form-control" id="cc-cvv" placeholder="" required/>
-                <div className="invalid-feedback">
-                  Security code required
-                </div>
-              </div>
-            </div>
-            <button className="btn btn-primary btn-lg btn-block" type="submit">Continue to checkout</button>
-          </form>
-        </div>
-      </div>
     )
   }
 }
